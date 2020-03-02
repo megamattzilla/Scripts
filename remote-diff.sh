aca9bd82e9d2ff5ba7ecdc8aee3b6618f1f66861
Megamattzilla <45299091+megamattzilla@users.noreply.github.com>
2020-03-02 17:49:14 -0600

    Update remote-diff.sh

--- remote-diff.sh
+++ remote-diff.sh
@@ -3,6 +3,12 @@
 #second argument is command to execute on both local and remote systems. by default the data is sorted to make diff of large data sets easier. remote sort if needed. 
 #third argument is diff with desired flags
 ##example## ./remote-diff.sh root@192.168.1.247 "sessiondump -allkeys" "vimdiff" 
+
+#Run command on local system and send output to local-diff.txt
 $2 | sort > local-diff.txt
+
+#login to remote system, run command, and sent output to remote-diff.txt
 ssh $1 "$2" | sort > remote-diff.txt
+
+#compare local-diff and remote-diff 
 $3 local-diff.txt remote-diff.txt
